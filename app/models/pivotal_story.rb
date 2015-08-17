e74ac833d1e3da7571d4bfe7787a4c1767727011
Allan McLelland <awm@foraker.com>
2015-08-17 15:22:37 -0600

    Change cycle time to only use business days

--- app/models/pivotal_story.rb
+++ app/models/pivotal_story.rb
@@ -6,17 +6,37 @@ class PivotalStory < ActivePivot::Story
   end
 
   def duration
+    (accepted_at - started_at).to_i - weekends_time
+  end
+
+  def weekends_time
+    weekends_count * 1.day.to_i
+  end
+
+  def weekends_count
+    date_range.reject {|d| (1..5).include?(d.wday) }.length
+  end
+
+  def date_range
+    (started_at.to_date..accepted_at.to_date)
+  end
+
+  def duration_all_days
     (accepted_at - started_at).to_i
   end
 
   private
 
   def self.accepted_last_month(project_id)
-    for_project(project_id).started.accepted.last_month
+    for_project(project_id).feature.started.accepted.last_month
   end
 
   def self.average_duration_seconds (stories)
     stories.length > 0 ? stories.to_a.sum(&:duration) / stories.length : 0
+  end
+
+  def self.feature
+    where(story_type: 'feature')
   end
 
   def self.started
