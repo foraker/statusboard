1f75645908c44e62b90a051bf75129c589c026aa
Allan McLelland <awm@foraker.com>
2015-08-19 13:44:44 -0600

    rename project_id to pivotal_id

--- app/models/pivotal_project.rb
+++ app/models/pivotal_project.rb
@@ -1,23 +1,19 @@
 class PivotalProject < ActivePivot::Project
   validates :pivotal_id, :name, presence: true
 
-  def self.with_id(project_ids)
-    where(pivotal_id: project_ids)
+  def self.with_id(pivotal_ids)
+    where(pivotal_id: pivotal_ids)
   end
 
   def self.ordered_by_name
     order(:name)
   end
 
-  def self.name_by_id(project_id)
-    find_by_pivotal_id(project_id).name
+  def self.name_by_id(pivotal_id)
+    find_by_pivotal_id(pivotal_id).name
   end
 
   def average_cycle_time
     PivotalStory.month_cycle_time(pivotal_id)
-  end
-
-  def cycle_time
-    duration_in_words(average_cycle_time)
   end
 end
